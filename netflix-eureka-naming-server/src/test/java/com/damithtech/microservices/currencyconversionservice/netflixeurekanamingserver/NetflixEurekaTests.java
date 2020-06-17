734ff736ec0d56d9ace69da7fa2a8491a153e5e1
damithn <damithnsamarakoon@gmail.com>
2020-06-18 03:06:49 +0530

    Adding eureka naming server

---
+++ netflix-eureka-naming-server/src/test/java/com/damithtech/microservices/currencyconversionservice/netflixeurekanamingserver/NetflixEurekaTests.java
@@ -0,0 +1,13 @@
+package com.damithtech.microservices.currencyconversionservice.netflixeurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetflixEurekaTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
