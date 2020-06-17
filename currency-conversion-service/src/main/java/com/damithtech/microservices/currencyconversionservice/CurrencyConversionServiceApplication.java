752f56621d0ae9134dc2f334fcd9ed9cd4cba4c4
damithn <damithnsamarakoon@gmail.com>
2020-06-18 02:51:46 +0530

    regiserd with eureka namin server

--- currency-conversion-service/src/main/java/com/damithtech/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ currency-conversion-service/src/main/java/com/damithtech/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -2,11 +2,15 @@ package com.damithtech.microservices.currencyconversionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 
 @EnableFeignClients("com.damithtech.microservices.currencyconversionservice")
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableEurekaClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
